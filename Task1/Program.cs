edf89e46000a10889db0416a66b6814aaa23e989
Name <email@example.ru>
2022-11-29 06:30:48 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var watch = Stopwatch.StartNew();
+            //List
+            var list = File.ReadLines("C:\\Users\\RAZAR\\Desktop\\Text1.txt").ToList();
+            Console.WriteLine($"List: {watch.Elapsed.TotalMilliseconds} ms");
+            //LinkedList
+            var lList = new LinkedList<string>();
+            lList.AddFirst(File.ReadAllText("C:\\Users\\RAZAR\\Desktop\\Text1.txt"));
+            Console.WriteLine($"LinkedList: {watch.Elapsed.TotalMilliseconds} ms");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\RAZAR\\Desktop\\Text1.txt");
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray())
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(x => x)
+                .Select(x => new { Word = x.Key, Count = x.Count() })
+                .OrderByDescending(x => x.Count)
+                .Take(10);
+            foreach (var word in noPunctuationText)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
